ed9fc30448c384f68f168ba653d5775141ab737b
Alexei Khatskevich <alexei.khatskevich@adesso.de>
2018-08-14 11:43:14 +0200

    Jenkins pipeline

---
+++ jenkinsfile.groovy
@@ -0,0 +1,30 @@
+node {
+   def mvnHome
+   stage('Preparation') { 
+      // Get some code from a GitHub repository
+      //git https://github.com/Gmugra/net.cactusthorn.utils.git
+      //git branch: 'master', credentialsId: '8a2c5006-46dc-4a63-8559-d12dcaeb1528', url: 'https://github.com/Gmugra/net.cactusthorn.utils.git'
+      git branch: 'master', url: 'https://github.com/Gmugra/net.cactusthorn.utils.git'
+       
+      mvnHome = tool 'Maven 3.5.3'
+   }
+   stage('Checkstyle') {
+      if (isUnix()) {
+         sh "'${mvnHome}/bin/mvn' checkstyle:check"
+      } else {
+         bat(/"${mvnHome}\bin\mvn" checkstyle:check/)
+      }
+   }
+   stage('Build') {
+      if (isUnix()) {
+         sh "'${mvnHome}/bin/mvn' checkstyle:check"
+      } else {
+         //bat(/"${mvnHome}\bin\mvn" -Dmaven.test.failure.ignore clean package/)
+         bat(/"${mvnHome}\bin\mvn" clean package/)
+      }
+   }
+   stage('Results') {
+        junit '**/target/surefire-reports/TEST-*.xml'
+        archiveArtifacts artifacts: 'target/*.jar', fingerprint: true
+   }
+}
