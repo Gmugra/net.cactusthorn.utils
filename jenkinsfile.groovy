285ce85fa9607071d54cf8cae8fa92066dc019f8
Alexei Khatskevich <alexei.khatskevich@adesso.de>
2018-08-14 13:26:16 +0200

    Jenkins pipeline small improvements

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,26 +1,26 @@
 node {
    def mvnHome
-   stage('Preparation') { 
+   stage('Preparation') {
       // Get some code from a GitHub repository
       //git https://github.com/Gmugra/net.cactusthorn.utils.git
       //git branch: 'master', credentialsId: '8a2c5006-46dc-4a63-8559-d12dcaeb1528', url: 'https://github.com/Gmugra/net.cactusthorn.utils.git'
       git branch: 'master', url: 'https://github.com/Gmugra/net.cactusthorn.utils.git'
-       
+
       mvnHome = tool 'Maven 3.5.3'
    }
    stage('Checkstyle') {
       if (isUnix()) {
-         sh "'${mvnHome}/bin/mvn' checkstyle:check"
+         sh "'${mvnHome}/bin/mvn' -batch-mode -U -e checkstyle:check"
       } else {
-         bat(/"${mvnHome}\bin\mvn" checkstyle:check/)
+         bat(/"${mvnHome}\bin\mvn" -batch-mode -U -e checkstyle:check/)
       }
    }
    stage('Build') {
       if (isUnix()) {
-         sh "'${mvnHome}/bin/mvn' checkstyle:check"
+         sh "'${mvnHome}/bin/mvn' -batch-mode -U -e checkstyle:check"
       } else {
          //bat(/"${mvnHome}\bin\mvn" -Dmaven.test.failure.ignore clean package/)
-         bat(/"${mvnHome}\bin\mvn" clean package/)
+         bat(/"${mvnHome}\bin\mvn" -batch-mode -U -e clean package/)
       }
    }
    stage('Results') {
--- src/main/java/net/cactusthorn/utils/crc32/CRC32.java
+++ src/main/java/net/cactusthorn/utils/crc32/CRC32.java
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * Copyright (C) 2017, Alexei Khatskevich
  * All rights reserved.
- * 
+ *
  * Licensed under the BSD 2-clause (Simplified) License (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
